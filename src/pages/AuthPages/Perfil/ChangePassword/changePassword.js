7b0a956c0efa0fb0ebbfc48bd7b68282769787f6
Lucas Fasolato <fasolatolucas14@gmail.com>
2022-07-17 19:11:16 -0300

    Change logic changePassword

--- src/pages/AuthPages/Perfil/ChangePassword/changePassword.js
+++ src/pages/AuthPages/Perfil/ChangePassword/changePassword.js
@@ -28,9 +28,6 @@ function Changepassword() {
             setCargandoUser(false);
             setUser(res)
             console.log(res)
-            toast.success("Usuario cargado correctamente", {
-                position: toast.POSITION.BOTTOM_RIGHT,
-            });
         })
     }, [])
 
@@ -44,48 +41,46 @@ function Changepassword() {
         toast.error("Contraseña no modificada", {
             position: toast.POSITION.BOTTOM_RIGHT,
         });
+        setTimeout(() => setModificandoPassword(false) , 100);
     }
 
 
     const modifyPassword = (e) => {
         setModificandoPassword(true);
-        if((newPassword==="")||(reNewPassword==="")||(newPassword.length < 8)||(reNewPassword.length < 8)) {
-            toast.error("Error. La contraseña debe contener mínimo 8 caracteres.", {
+        // CAMBIAR ESTO, PONGO MAIL PQ NO SE COMO TRAER LA CONTRASEÑA
+        if(String(actualPassword)!==String(user.mail)) {
+            toast.warning("La contraseña actual no es correcta.", {
                 position: toast.POSITION.BOTTOM_RIGHT,
             });
-        }
-        else {
-            // CAMBIAR ESTO, PONGO MAIL PQ NO SE COMO TRAER LA CONTRASEÑA
-            if((newPassword===reNewPassword) && (actualPassword===user.mail)) {
-                e.preventDefault()
-                httpPut('users/edit/me', {password: newPassword})
-                    .then((res) => {
-                        setModificandoPassword(false)
-                        console.log(res)
-                        if (res.request.status === 201) {
-                            changed()
-                        } else {
-                            rechazada()
-                        }
-                    }).catch((err)=> alert('No se ha podido modificar la contraseña.',{
-                        type: "error"
-                    }))
-            } 
-            else {
-                if(newPassword!==reNewPassword)
-                {
-                    toast.error("Error. Las contraseñas no son iguales. Confirme el ingreso de su nueva contraseña", {
+            setTimeout(() => setModificandoPassword(false) , 100);
+        } else {
+            if(String(newPassword)!==String(reNewPassword))
+            {
+                toast.warning("Las contraseñas no son iguales. Confirme el ingreso de su nueva contraseña", {
+                    position: toast.POSITION.BOTTOM_RIGHT,
+                });
+                setTimeout(() => setModificandoPassword(false) , 100);
+            } else {
+                if((newPassword.length < 8)||(reNewPassword.length < 8)) {
+                    toast.warning("La contraseña debe contener mínimo 8 caracteres.", {
                         position: toast.POSITION.BOTTOM_RIGHT,
                     });
+                    setTimeout(() => setModificandoPassword(false) , 100);
+                } else {
+                    if((newPassword===reNewPassword) && (String(actualPassword)===String(user.mail))) {
+                        e.preventDefault()
+                        httpPut('users/edit/me', {password: String(newPassword)}, false)
+                            .then((res) => {
+                                setModificandoPassword(false)
+                                console.log(res)
+                                if ((res.request.status === 201) || (res.request.status === 200) ){
+                                    changed();
+                                } else {
+                                    rechazada();
+                                }
+                            }).catch((err)=> rechazada())
+                    } 
                 }
-                else {
-                    // CAMBIAR ESTO, PONGO MAIL PQ NO SE COMO TRAER LA CONTRASEÑA
-                    if(actualPassword!==user.mail) {
-                        toast.error("Error. La contraseña actual no es correcta.", {
-                            position: toast.POSITION.BOTTOM_RIGHT,
-                        });
-                    }
-                }  
             }
         }
       }
@@ -115,7 +110,6 @@ function Changepassword() {
                                     <input type="text" className='changePassword_column-2-transf-input' placeholder='Reingrese la contraseña nueva' 
                                     value={reNewPassword} onChange={(e) => setReNewPassword(e.target.value)}/>
                                 </div>
-                                
                             </div>
                             <div className='changePassword_column-2-bttn-enviar'>
                                 <button type='submit' className='changePassword_column-2-bttn-enviar-bttn'>CONFIRMAR CONTRASEÑA</button>
@@ -123,14 +117,13 @@ function Changepassword() {
                         </form>
                         }
                         {modificandoPassword &&
-                        <div className='changePassword_column-2-transf-title'>
+                        <div className='changePassword_column-2-transf-modificando'>
                             <h2 className='changePassword_column-2-transf-h2'>Modificando contraseña...</h2>
-                            <div className="creando-changePassword-loading">
-                                <PropagateLoader color={color} loading={modificandoPassword} size={17}/>
+                            <div className="changePassword-loading">
+                                <PropagateLoader color={color} loading={modificandoPassword} size={10}/>
                             </div>
                         </div>
                         }
-                        
                     </section>
                     <div className='transferirdinero_column-2-bttn-volver'>
                         <button className='transferirdinero_column-2-bttn-volver-bttn' onClick={() => navigate("/perfil")}>VOLVER A MI PERFIL</button>
