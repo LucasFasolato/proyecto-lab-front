d0caaf5a78042ac0108e4eaccaa2592caaaed8ec
LucasFasolato <fasolatolucas14@gmail.com>
2022-11-22 19:01:50 -0300

    Fix screen6 styles, comments responsive

--- src/components/Comments/comments.js
+++ src/components/Comments/comments.js
@@ -3,6 +3,7 @@ import StarComment from '../StarComment/starComment'
 import './comments.css'
 
 function Comments({res}) {
+    console.log(res)
   return (
     <div className='comment_box-size'>
         <article className='comment_sp'>
@@ -10,22 +11,21 @@ function Comments({res}) {
         </article>
         <div className='display_content'> 
             <div className='display_user-info'>
-            <section className='user_img-sp'>
-                <img className='user_img-size' src="https://cdn.pixabay.com/photo/2016/06/15/16/16/man-1459246_960_720.png" alt='FOTO PERFIL USUARIO'/>
-            </section>
-            <div className='display_user-name'>
-                <article className='name_sp'>
-                    <h1>{res.usuario.username}</h1>
-                </article>  
-                <article className='user_sp'>
-                    <h2>{res.usuario.mail}</h2>
-                </article> 
+                <section className='user_img-sp'>
+                    <img className='user_img-size' src={res.usuario.avatar} alt='FOTO PERFIL USUARIO'/>
+                </section>
+                <div className='display_user-name'>
+                    <article className='name_sp'>
+                        <h1>{res.usuario.username}</h1>
+                    </article>  
+                    <article className='user_sp'>
+                        <h2>{res.usuario.mail}</h2>
+                    </article> 
+                </div>
             </div>
-            
-        </div>
-        <div className='display_star-info'>
-            <StarComment res={res.puntuacion}/>
-        </div>
+            <div className='display_star-info'>
+                <StarComment res={res.puntuacion}/>
+            </div>
         </div>
         
          
